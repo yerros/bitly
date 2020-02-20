8dce3b7e62ed0504f043fb019124081fa080048f
Yeris <bahtiaryeris@gmail.com>
2020-02-20 12:46:49 +0700

    Updates

---
+++ precache-manifest.83878e888defbdc2b02ff62220a0b31e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31d91e79e0772303a5fdfeddfd92de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f08444059a32133bc23",
+    "url": "/static/css/main.c45aeaba.chunk.css"
+  },
+  {
+    "revision": "343d7690cde671a4e14d",
+    "url": "/static/js/2.29d999b3.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/static/js/2.29d999b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f08444059a32133bc23",
+    "url": "/static/js/main.9cdac3cd.chunk.js"
+  },
+  {
+    "revision": "fa248aa0db6c809d3b52",
+    "url": "/static/js/runtime-main.99a09b66.js"
+  },
+  {
+    "revision": "75f96c472febe77392906fd6bf253f66",
+    "url": "/static/media/bg.75f96c47.jpg"
+  }
+]);
